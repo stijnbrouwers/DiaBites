7ed17268f700e02d062962167ae8adf2fb1194f9
Stijn Brouwers <stijnbrouwers@gmail.com>
2020-05-17 17:38:38 +0200

    Create project (typescript)

---
+++ ios/Pods/Target Support Files/Pods-DiaBites-DiaBitesTests/Pods-DiaBites-DiaBitesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaBites_DiaBitesTests : NSObject
+@end
+@implementation PodsDummy_Pods_DiaBites_DiaBitesTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-DiaBites-tvOSTests/Pods-DiaBites-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaBites_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_DiaBites_tvOSTests
+@end
